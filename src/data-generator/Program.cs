de52a8b829a07b617b44ae3464ca003cf8b91a58
DragonsLord <zheka2797@gmail.com>
2024-04-01 00:47:15 +0300

    port data generation solution

--- src/data-generator/Program.cs
+++ src/data-generator/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Text;
+
+var rowCount = 1_000_000_000;
+
+Random rand = new();
+var startTime = Stopwatch.GetTimestamp();
+
+// Write the string array to a new file named "WriteLines.txt".
+using (
+    StreamWriter outputFile =
+        new(Path.Combine("data", "measurements.txt"), true, Encoding.UTF8, 1048576)
+)
+{
+    for (int i = 0; i < rowCount; i++)
+    {
+        WeatherStation station = Stations.All[rand.Next(Stations.All.Count)];
+        await outputFile.WriteAsync(station.Name);
+        await outputFile.WriteAsync($";{station.Measurement(rand):0.0}");
+        await outputFile.WriteAsync('\n');
+    }
+}
+
+var elapsedTime = Stopwatch.GetElapsedTime(startTime);
+
+Console.WriteLine($"Completed: {elapsedTime}");
